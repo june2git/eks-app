7625446856aae908512da89d5f7eb560fabb9664
JuHyun BAEK(백주현) <june2soul@qoo10.com>
2025-10-22 20:14:31 +0900

    수정 (build trigger)

--- demo/src/main/java/com/example/demo/DemoController.java
+++ demo/src/main/java/com/example/demo/DemoController.java
@@ -18,4 +18,10 @@ public class DemoController {
     public String health() {
         return "OK";
     }
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "test";
+    }
 }
